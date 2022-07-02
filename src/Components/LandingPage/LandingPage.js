64129a2f19ad29057ca6efa99bfd7d726a4d82f4
presleyswilliam <presleyswilliam@gmail.com>
2022-07-02 14:47:43 -0400

    initializing files

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Gameboard from "../Gameboard/Gameboard.js";
+// import HANDROW from NEW COMPONENT
+
+export default function LandingPage (props) {
+/* API */
+    
+
+/* Variables */
+
+    
+/* Functions */
+    // handleClicks
+
+/* JSX */
+    let landingPageJSX;
+    landingPageJSX = <div>Landing Page</div>;
+
+    return (
+        <div>
+            {landingPageJSX}
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+window.sequenceApp = {};
+
+// window.sequenceApp.boardState = Array(10).fill(0).map(x => Array(10).fill(0));
+// window.sequenceApp.sequences = Array(4).fill(0).map(x => Array(5).fill(0).map(y => Array(2).fill(0)));
+
+function detectMobile() {
+  return ( ( window.innerWidth <= 800 ) /*&& ( window.innerHeight <= 600 )*/ );
+}
+window.sequenceApp.isMobile = detectMobile();
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
