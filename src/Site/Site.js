49f0531b61654d5cd08025cf2199822691ebc809
presleyswilliam <presleyswilliam@gmail.com>
2022-07-24 18:25:47 -0400

    troubleshooting games route for home screen icon

--- src/Site/Site.js
+++ src/Site/Site.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import GamesApp from './GamesApp/GamesApp';
 
 
