63bcc613c1cea6b67eb7f9d1fa5618910dc2bfc5
presleyswilliam <presleyswilliam@gmail.com>
2022-08-21 13:04:03 -0400

    adding nodemon and ts-node to dev environment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const env = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -10,7 +10,8 @@ const server = app.listen(PORT);
 
 app.use(express.json({}));
 
-require('./build_node/SocketLogic/games')(server);
+const serverImportString = (ENV !== 'development') ? './build_node/SocketLogic/games' : './server/SocketLogic/games';
+require(serverImportString)(server);
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
